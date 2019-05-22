a0de121bda09ba6ae93fba4e669b68ee41268df9
Nikolas <nikolas.gottschalk@gmail.com>
2019-05-22 13:36:12 +0200

    Adds Author resource

---
+++ app/Author.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+class Author extends Model
+{
+    protected $guarded = [];
+
+    protected $dates = ['dob'];
+
+    public function setDobAttribute($attribute)
+    {
+        $this->attributes['dob'] = Carbon::parse($attribute);
+    }
+}
